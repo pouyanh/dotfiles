2c5508de2aba7cc1deba57f98eadedde39ea3526
Pouyan Heyratpour <pouyan@janstun.com>
2019-04-14 15:15:06 +0430

    Emacs: Editorconfig
    Signed-off-by: Pouyan Heyratpour <pouyan@janstun.com>

--- emacs/init.el
+++ emacs/init.el
@@ -61,8 +61,7 @@ There are two things you can do about this warning:
      (deprecated :strike-through "#a9b7c6"))))
  '(package-selected-packages
    (quote
-    ;; (company-shell company-c-headers company-erlang company-lua company-php company-go company flycheck auto-complete cuda-mode go-mode yaml-mode php-mode markdown-mode dockerfile-mode docker-compose-mode golint flymake-go go-autocomplete auto-complete-c-headers go-eldoc darcula-theme))))
-    (company-shell company-c-headers company-erlang company-lua company-php company-go company flycheck cuda-mode go-mode yaml-mode php-mode markdown-mode dockerfile-mode docker-compose-mode golint flymake-go go-eldoc darcula-theme))))
+    (editorconfig company-shell company-c-headers company-erlang company-lua company-php company-go company flycheck cuda-mode go-mode yaml-mode php-mode markdown-mode dockerfile-mode docker-compose-mode golint flymake-go go-eldoc darcula-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -78,6 +77,10 @@ There are two things you can do about this warning:
 
 (savehist-mode 1)
 (desktop-save-mode 1)
+
+;; Editorconfig
+
+(editorconfig-mode 1)
 
 ;; Company
 
